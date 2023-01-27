82c99a5a47c998d85e036485200aecfcc5ee3a48
Allan <Allanlogz@gmail.com>
2023-01-28 00:02:55 +0300

    Routes

---
+++ src/components/Routes.js
@@ -0,0 +1,5 @@
+export {default as Video} from "./Video";
+export {default as Channel} from "./Channel";
+export {default as Feed} from "./Feed";
+export {default as Navbar} from "./Navbar";
+export {default as Search} from "./Search";
